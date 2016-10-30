bd83f1823192f9387ab0481348853b4ad88094dc
teddybear02 <theo.frohlich@gmail.com>
2016-10-30 02:15:37 -0400

    Revise code and allow smoothing based on property-value pair

--- src/findVel.m
+++ src/findVel.m
@@ -7,17 +7,30 @@
 %
 % Author: Ted Frohlich <ttf10@case.edu>
 %
-function v = findVel(x, t, tbounds)
+function v = findVel(x, t, tbounds, varargin)
   
   % Find bound indices
   i1 = find(t > tbounds(1), 1);
   i2 = find(t < tbounds(2), 1, 'last');
   
-  % Compute time step
-  dt = t(2) - t(1);
+  % Compute numerical differences (within sample)
+  xs = x(i1:i2);    dx = diff(xs);
+  ts = t(i1:i2);    dt = diff(ts);
   
-  % Compute differnce
-  v = (x(i1+1:i2) - x(i1:i2-1)) / dt;
-%   v = smoothData(v, 2);
+  % Compute velocity vector (within sample)
+  v = dx ./ dt;
+  
+  % Smoothen data, if called for
+  if ~isempty(varargin)
+    property = varargin{1};
+    value    = varargin{2};
+    if strcmp(property,'Smoothing')
+      if isnumeric(value)
+        v = smoothData(v, value);
+      elseif strcmp(value,'auto')
+        v = smoothData(v, 2);
+      end
+    end
+  end
   
 end
