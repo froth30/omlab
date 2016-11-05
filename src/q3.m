e5592ae8c165bbece7370409090f69b456913c9e
teddybear02 <theo.frohlich@gmail.com>
2016-11-05 02:48:15 -0400

    Retroactively add Question 3 code

---
+++ src/q3.m
@@ -0,0 +1,15 @@
+%
+%                    Case Western Reserve University
+%
+%                                EBME 318
+%                   Biomedical Engieering Laboratory I
+%                               Fall 2016
+%
+% Author: Ted Frohlich <ttf10@case.edu>
+%
+
+clear
+
+% set(figure(30), 'Name','Question 3: Imaginary Tracking')
+
+dataPlotter  % Trials 9-10
