d7c229f913b7ab745f3eaaab7f294e40bfd04777
teddybear02 <theo.frohlich@gmail.com>
2016-11-06 14:31:48 -0500

    Place Question 4 code in its own file

---
+++ src/q4.m
@@ -0,0 +1,15 @@
+%
+%                    Case Western Reserve University
+%
+%                                EBME 318
+%                   Biomedical Engieering Laboratory I
+%                               Fall 2016
+%
+% Author: Ted Frohlich <ttf10@case.edu>
+%
+
+clear
+% set(figure(40), 'Name','Question 4: Sustained Saccadic Oscillations')
+
+dataPlotter(18);  ylim([-15 15])
+title('Sustained Saccadic Oscillations')
